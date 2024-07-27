88fb55d92cccc46f7ea21286f957c2afa8672757
Tahmid Shakil <at.shakil.92@gmail.com>
2024-07-27 13:37:45 +0700

    Update script

--- sh/install_al2_arm.sh
+++ sh/install_al2_arm.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 curl -sS https://raw.githubusercontent.com/atshakil/toolkit/master/sh/mongo_al2_arm.sh | sh
 curl -sS https://raw.githubusercontent.com/atshakil/toolkit/master/sh/mosh_al2.sh | sh
 curl -sS https://raw.githubusercontent.com/atshakil/toolkit/master/sh/redis_al2.sh | sh
