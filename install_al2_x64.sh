24fc9f238901403931bb86fafd9652227c2c6da3
Tahmid Shakil <at.shakil.92@gmail.com>
2024-07-27 13:29:59 +0700

    Add batch install script

---
+++ install_al2_x64.sh
@@ -0,0 +1,3 @@
+curl -sS https://raw.githubusercontent.com/atshakil/toolkit/master/sh/mongo_al2_x64.sh | sh
+curl -sS https://raw.githubusercontent.com/atshakil/toolkit/master/sh/mosh_al2.sh | sh
+curl -sS https://raw.githubusercontent.com/atshakil/toolkit/master/sh/redis_al2.sh | sh
